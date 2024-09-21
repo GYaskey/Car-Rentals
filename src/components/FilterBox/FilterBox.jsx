d769bc313f7c70cc9bdbb72c2b00cb127ad72d11
Gleb Yasynskyi <gleb.yasinsky@gmail.com>
2024-09-21 15:39:48 +0300

    filter styles

--- src/components/FilterBox/FilterBox.jsx
+++ src/components/FilterBox/FilterBox.jsx
@@ -9,9 +9,8 @@ import { getAllCarsThunk } from '../../redux/filters/operations';
 import { filterCars, resetFilter } from '../../redux/filters/slice';
 import { makeFilterOptions } from '../../utilities/makeFilterOptions';
 import { priceFilterOptions } from '../../utilities/priceFilterOptions';
-import { makeSelectorStyles } from '../../utilities/makeSelectorStyles';
-import { priceSelectorStyles } from '../../utilities/priceSelectorStyles';
 import s from './FilterBox.module.css';
+import clsx from 'clsx';
 
 const FilterBox = () => {
   const dispatch = useDispatch();
@@ -81,36 +80,40 @@ const FilterBox = () => {
       {({ setFieldValue }) => (
         <Form className={s.form}>
           <div className={s.filterContainer}>
-            <div className={s.filter}>
-              <label htmlFor="makeFilter">Car brand</label>
+            <div className={s.makeFilter}>
+              <label htmlFor="makeFilter" className={s.selectLabel}>
+                Car brand
+              </label>
               <Field name="makeFilter">
                 {({ field }) => (
                   <Select
-                    className={s.select}
+                    className={clsx(s.customSelect, s.makeSelect)}
+                    classNamePrefix="react-select"
                     options={makeOptions}
                     name="makeFilter"
                     placeholder="Enter the text"
                     onChange={option => setFieldValue(field.name, option)}
                     value={field.value}
-                    styles={makeSelectorStyles}
                     isClearable
                   />
                 )}
               </Field>
             </div>
 
-            <div className={s.filter}>
-              <label htmlFor="maxPrice">Price / 1 hour</label>
+            <div className={s.priceFilter}>
+              <label htmlFor="maxPrice" className={s.selectLabel}>
+                Price / 1 hour
+              </label>
               <Field name="maxPrice">
                 {({ field }) => (
                   <Select
-                    className={s.select}
+                    className={clsx(s.customSelect, s.priceSelect)}
+                    classNamePrefix="react-select"
                     options={priceOptions}
                     name="maxPrice"
                     placeholder="To $"
                     onChange={option => setFieldValue(field.name, option)}
                     value={field.value}
-                    styles={priceSelectorStyles}
                     isClearable
                   />
                 )}
@@ -118,32 +121,38 @@ const FilterBox = () => {
             </div>
 
             <div className={s.mileageContainer}>
-              <p>Сar mileage / km</p>
-              <div className={s.mileageInput}>
-                <Field name="minMileage">
-                  {({ field }) => (
-                    <input
-                      className={s.input}
-                      type="number"
-                      placeholder="From"
-                      {...field}
-                      value={field.value || ''}
-                    />
-                  )}
-                </Field>
-              </div>
-              <div className={s.mileageInput}>
-                <Field name="maxMileage">
-                  {({ field }) => (
-                    <input
-                      className={s.input}
-                      type="number"
-                      placeholder="To"
-                      {...field}
-                      value={field.value || ''}
-                    />
-                  )}
-                </Field>
+              <p className={s.mileageLabel}>Сar mileage / km</p>
+              <div className={s.mileageInputs}>
+                <div className={s.mileageInput}>
+                  <Field name="minMileage">
+                    {({ field }) => (
+                      <div className={s.inputWrapper}>
+                        <span className={s.inputLabel}>From</span>
+                        <input
+                          className={clsx(s.input, s.minInput)}
+                          type="number"
+                          {...field}
+                          value={field.value || ''}
+                        />
+                      </div>
+                    )}
+                  </Field>
+                </div>
+                <div className={s.mileageInput}>
+                  <Field name="maxMileage">
+                    {({ field }) => (
+                      <div className={s.inputWrapper}>
+                        <span className={s.inputLabel}>To</span>
+                        <input
+                          className={clsx(s.input, s.maxInput)}
+                          type="number"
+                          {...field}
+                          value={field.value || ''}
+                        />
+                      </div>
+                    )}
+                  </Field>
+                </div>
               </div>
             </div>
           </div>
